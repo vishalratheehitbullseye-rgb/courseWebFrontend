8d9a3bd59f34f57b6734acaa0741cb24e1a0eb41
vishalratheehitbullseye-rgb <vishalrathee.hitbullseye@gmail.com>
2025-12-29 15:36:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import CourseDetails from "./pages/CourseDetails.jsx";
+import Orders from "./pages/Orders.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/course/:courseId" element={<CourseDetails />} />
+      <Route path="/orders/" element={<Orders />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
